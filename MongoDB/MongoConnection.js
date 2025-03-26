18da7bc55faa00326e2509bcb48238e878e243c8
tankvaishali <vaishalitank28603@gmail.com>
2025-03-26 16:26:09 +0530

    change mongo string

--- MongoDB/MongoConnection.js
+++ MongoDB/MongoConnection.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const MongoDB = () => {
-  mongoose.connect("mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/solardb?retryWrites=true&w=majority");
+  // mongoose.connect("mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/solardb?retryWrites=true&w=majority");
+  mongoose.connect("mongodb+srv://vaishalitank28603:v1a2i3s4h5a6l7i8@cluster0.qzxy1.mongodb.net/solardb?retryWrites=true&w=majority");
   console.log("Connected to MongoDB!");
-
 };
 
 export default MongoDB;
